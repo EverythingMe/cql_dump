5715c8260288954017fd1fabfd760a0ab29182b6
Timor Raiman <timor@everything.me>
2014-06-05 18:40:56 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'cql_dump',
-    version = '0.1b',
+    version = '0.2b',
     install_requires = ['cassandra-driver>=2.0.1'],
     scripts = ['bin/cql_dump.py'],
     description = 'A CLI utility for creating dumps of cassandra databases',
