465259e88932bf0632ae1d6db963efd424d2e126
Timor Raiman <timor@everything.me>
2014-06-05 14:14:22 +0300

    Add download_url for pipy distrib

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     scripts = ['bin/cql_dump.py'],
     description = 'A CLI utility for creating dumps of cassandra databases',
     url = 'https://github.com/EverythingMe/cql_dump',
+    download_url = 'https://github.com/EverythingMe/cql_dump/tarball/0.1',
     author = 'Timor Raiman',
     author_email = 'timor@everything.me'
 )
